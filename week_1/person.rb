7a4aba86f030c28a24c98c9254c431922393528f
Miah Walley <miah74me@gmail.com>
2014-06-09 11:50:05 -0500

    Adds person.rb

---
+++ week_1/person.rb
@@ -0,0 +1,32 @@
+loop = true
+People = []
+
+while(loop)
+puts "Would you like to add a person?"
+more = gets.chop
+
+  if more == "yes"
+
+  puts "Please enter a first name for the person"
+  first_name = gets.chop
+
+  puts "Please enter a last name for the person"
+  last_name = gets.chop
+
+  puts "Please enter the age of the person"
+  age = gets.chop.to_i
+
+  person = {
+  first_name: "#{first_name}",
+  last_name: "#{last_name}",
+  age: "#{age}"
+  }
+
+  People.push(person)
+  
+  else
+    loop = false
+  end
+end
+
+puts "You added #{People.length} people."
