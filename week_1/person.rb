1c4ff522a60d6ee63c08a93b5af3ace15996b733
Miah Walley <miah74me@gmail.com>
2014-06-09 15:27:24 -0500

    Adds person.rb

--- week_1/person.rb
+++ week_1/person.rb
@@ -7,26 +7,24 @@ more = gets.chop
 
   if more == "yes"
 
-  puts "Please enter a first name for the person"
-  first_name = gets.chop
+    puts "Please enter a first name for the person"
+    first_name = gets.chop
 
-  puts "Please enter a last name for the person"
-  last_name = gets.chop
+    puts "Please enter a last name for the person"
+    last_name = gets.chop
 
-  puts "Please enter the age of the person"
-  age = gets.chop.to_i
+    puts "Please enter the age of the person"
+    age = gets.chop.to_i
 
-  person = {
-  first_name: "#{first_name}",
-  last_name: "#{last_name}",
-  age: "#{age}"
-  }
-
-  people.push(person)
-  
+    person = {
+    first_name: "#{first_name}",
+    last_name: "#{last_name}",
+    age: "#{age}"
+    }
+    people.push(person)
   else
     loop = false
   end
 end
 
-puts "You added #{People.length} people."
+puts "You added #{people.length} people."
