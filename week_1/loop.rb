acbd26777b31f357fbad137e7b8dc539ada9bcd5
Miah Walley <miah74me@gmail.com>
2014-06-09 23:25:35 -0500

    Update to loop.rb

--- week_1/loop.rb
+++ week_1/loop.rb
@@ -1,9 +1,20 @@
 loop = true
 
-while(loop) do
+ while(loop) do
   puts "Put some stuff here" 
   string = gets.chop
   if string == ""
-    loop = false
+   loop = false
   end
-end
+  if string.to_i.to_s == string
+    if string.to_i % 2 == 0
+      type = "an even number"
+    else
+      type = "an odd number"
+    end
+  else
+    type = "a string"
+  end
+   puts "You entered #{string} which is #{type}"  
+ end
+
