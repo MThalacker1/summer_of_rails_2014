24a876e82a7a589af757eb88cd6ffa0515cc1c5a
Miah Walley <miah74me@gmail.com>
2014-06-12 12:23:14 -0500

    Adds Interest module

--- week_2/bank_account.rb
+++ week_2/bank_account.rb
@@ -1,6 +1,14 @@
+module Interest
+  def calculateInterest(balance, rate, time)
+    interest = @balance * (rate / 100) * time
+    sprintf("$%.2f",interest)
+  end
+end
+
 class BankAccount
   attr_reader :balance
   attr_accessor :name, :records, :msg
+  include Interest
 
   def deposit(amount)
     @balance += amount
@@ -40,13 +48,17 @@ class BankAccount
   end
 end
 
+  
 rcu = BankAccount.new
 rcu.deposit(400)
 rcu.withdraw(75)
 rcu.deposit(20)
 rcu.withdraw(60)
 puts "What would you like to name your account?"
-rcu.name = gets.chop 
+rcu.name = gets.chop
 puts rcu.history
-
-
+puts "Rate"
+rate = gets.chop.to_f
+puts "Time"
+time = gets.chop.to_i
+puts "The interest is #{rcu.calculateInterest(rcu.balance, rate, time)}"
